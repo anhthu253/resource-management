ee0a3c8d0d98f8cff6baaed992626f934c866a2c
anhthu253 <thu_nguyen@gmx.de>
2026-03-12 18:21:45 +0100

    add missing files which should belong to the previous commit

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'http://localhost:8081',
   stripePublishableKey:
     'pk_test_51SeCZ4KBERM5kDvzgOWUdtxx8xV7b5mVtr8hN9E1JVrF9YJNqDzUY4Zahz1Lz9AcdqqSRdhDDDXBNLpoxQLric6G00qooG1pnh',
