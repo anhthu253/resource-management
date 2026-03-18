2e8aad41c101c2a2e1192f579819af69a4267af4
anhthu253 <thu_nguyen@gmx.de>
2026-03-18 07:42:04 +0100

    Fix WebClient QUIC error by forcing HTTP/1.1

--- booking-service/src/main/java/com/example/springboot/service/WebClientConfig.java
+++ booking-service/src/main/java/com/example/springboot/service/WebClientConfig.java
@@ -5,17 +5,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.HttpProtocol;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 @ConfigurationProperties(prefix = "services.resource")
 @Setter
 public class WebClientConfig {
     private String url;
-
     @Bean("resourceWebClient")
     public WebClient resourceWebClient() {
+        HttpClient httpClient = HttpClient.create()
+                .protocol(HttpProtocol.HTTP11);
         return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .baseUrl(url)
                 .build();
     }
