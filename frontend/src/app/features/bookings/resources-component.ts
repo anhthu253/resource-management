d2de3cd21573395d68f8ea4e36695f38236f2195
anhthu253 <thu_nguyen@gmx.de>
2026-03-19 15:15:05 +0100

    UI edited: list all resources with base price and price unit in a table

--- frontend/src/app/features/bookings/resources-component.ts
+++ frontend/src/app/features/bookings/resources-component.ts
@@ -4,29 +4,42 @@ import {
   Component,
   DestroyRef,
   inject,
+  OnInit,
+  ViewChild,
 } from '@angular/core';
 import { ResourceDto } from '../../core/dtos/resource.dto';
 import { BookingService } from '../../core/services/booking.service';
-import { MatList, MatListItem } from '@angular/material/list';
 import { CommonModule } from '@angular/common';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   standalone: true,
   selector: 'app-resources',
   templateUrl: './resources-component.html',
-  imports: [MatList, MatListItem, CommonModule],
+  imports: [MatTableModule, MatPaginatorModule, CommonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: `
+    table {
+      width: 100%;
+    }
+  `,
 })
-export class ResourcesComponent {
-  resources!: ResourceDto[];
+export class ResourcesComponent implements OnInit {
   isLoading = false;
   message = '';
   private destroyRef = inject(DestroyRef);
+
+  dataSource!: MatTableDataSource<ResourceDto>;
+  displayedColumns: string[] = ['resourceName', 'basePrice', 'priceUnit'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   constructor(
     private bookingService: BookingService,
     private cdr: ChangeDetectorRef,
   ) {}
+
   ngOnInit(): void {
     this.isLoading = true;
     this.bookingService
@@ -35,15 +48,16 @@ export class ResourcesComponent {
       .subscribe({
         next: (res: ResourceDto[]) => {
           this.isLoading = false;
-          this.resources = res;
+          this.dataSource = new MatTableDataSource<ResourceDto>(res);
+          this.dataSource.paginator = this.paginator;
+          this.message = '';
           this.cdr.detectChanges();
         },
         error: (err) => {
           this.isLoading = false;
-          this.cdr.detectChanges();
           this.message =
             err.error || err.message || 'Failed to fetch resources. Please try again later.';
-          console.log(err);
+          this.cdr.detectChanges();
         },
       });
   }
