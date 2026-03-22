dafc0a3378aafe08defe2fbf7efbce06c71b8b3e
anhthu253 <thu_nguyen@gmx.de>
2026-03-22 18:22:39 +0100

    Refactor RabbitMQ consumer configuration to remove redundant topology declaration and rely on external queue setup.

--- notification-service/src/main/java/com/example/springboot/service/RabbitMQConfig.java
+++ notification-service/src/main/java/com/example/springboot/service/RabbitMQConfig.java
@@ -14,69 +14,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableRabbit
 public class RabbitMQConfig {
-    public static final String BOOKING_EXCHANGE = "booking.exchange";
-
-    @Bean
-    public TopicExchange bookingExchange() {
-        return new TopicExchange(BOOKING_EXCHANGE);
-    }
-
-    // Queues
-    @Bean
-    public Queue bookingCreatedQueue() {
-        return new Queue("booking.created.queue");
-    }
-    @Bean
-    public Queue bookingFailedQueue() {
-        return new Queue("booking.failed.queue");
-    }
-
-    @Bean
-    public Queue bookingCancelledQueue() {
-        return new Queue("cancel.succeeded.queue");
-    }
-
-    @Bean
-    public Queue cancelFailedQueue() {
-        return new Queue("cancel.failed.queue");
-    }
-
-    @Bean
-    public Queue modifySuccessQueue() {
-        return new Queue("modify.succeeded.queue");
-    }
-
-    @Bean
-    public Queue modifyFailedQueue() {
-        return new Queue("modify.failed.queue");
-    }
-
-    // Bindings
-    @Bean
-    public Binding bookingCreatedBinding(Queue bookingCreatedQueue, TopicExchange bookingExchange) {
-        return BindingBuilder.bind(bookingCreatedQueue).to(bookingExchange).with("BOOKING_CREATED");
-    }
-    @Bean
-    public Binding bookingFailedBinding(Queue bookingFailedQueue, TopicExchange bookingExchange) {
-        return BindingBuilder.bind(bookingFailedQueue).to(bookingExchange).with("BOOKING_FAILED");
-    }
-    @Bean
-    public Binding bookingCancelledBinding(Queue bookingCancelledQueue, TopicExchange bookingExchange) {
-        return BindingBuilder.bind(bookingCancelledQueue).to(bookingExchange).with("BOOKING_CANCELED");
-    }
-    @Bean
-    public Binding cancelFailedBinding(Queue cancelFailedQueue, TopicExchange bookingExchange) {
-        return BindingBuilder.bind(cancelFailedQueue).to(bookingExchange).with("BOOKING_CANCEL_FAILED");
-    }
-    @Bean
-    public Binding bookingModifiedBinding(Queue modifySuccessQueue, TopicExchange bookingExchange) {
-        return BindingBuilder.bind(modifySuccessQueue).to(bookingExchange).with("BOOKING_MODIFIED");
-    }
-
-    @Bean
-    public Binding modifyFailedBinding(Queue modifyFailedQueue, TopicExchange bookingExchange) {
-        return BindingBuilder.bind(modifyFailedQueue).to(bookingExchange).with("BOOKING_MODIFY_FAILED");
-    }
 
     @Bean
     public Jackson2JsonMessageConverter jsonMessageConverter() {
