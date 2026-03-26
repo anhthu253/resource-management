b2246bc8362e2dccc122698eded30942de5df627
anhthu253 <thu_nguyen@gmx.de>
2026-03-26 15:46:21 +0100

    remove redundancy

--- frontend/src/app/core/services/payment.service.ts
+++ frontend/src/app/core/services/payment.service.ts
@@ -1,24 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRoute,
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-} from '@angular/router';
-import { BookingStateService } from './booking.state.service';
+import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class PaymentGuard implements CanActivate {
   constructor(private router: Router) {}
   //users are only allowed to navigate form /new-booking to /payment. Prevent user from typing /payment directly
   canActivate(route: ActivatedRouteSnapshot): boolean {
-    const bookingId = route.queryParamMap.get('bookingId');
-    if (!bookingId) {
-      this.router.navigate(['/new-booking']);
-      return false;
-    }
-
-    return true;
+    this.router.navigate(['/new-booking']);
+    return false;
   }
 }
